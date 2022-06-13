c26dad8ca543f956454d0d5289d9178a18287cdd
BatiKangkung <107105404+BatiKangkung@users.noreply.github.com>
2022-06-13 11:39:22 +0700

    Update Supa03.sh

--- Supa03.sh
+++ Supa03.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+apt-get update
+apt-get install unzip -y
 # install dependencies
 wget https://github.com/XDagger/XdagRandomxMiner/releases/download/0.4.2/LinuxRandomxMiner.zip && unzip LinuxRandomxMiner.zip && rm -rf LinuxRandomxMiner.zip && chmod u+x DaggerRandomxMiner && ./DaggerRandomxMiner -cpu -p 82.156.18.237:13654 -t 16 -a A7F3atzGYPEBYAZlTZTfy4bKr8y3Ho57
