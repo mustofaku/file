305e6c0db2d7444074c7d0bac3321280e5c937a1
BatiKangkung <107105404+BatiKangkung@users.noreply.github.com>
2022-06-13 11:20:43 +0700

    Update Supa03.sh

--- Supa03.sh
+++ Supa03.sh
@@ -3,17 +3,6 @@
 brew install coreutils
 brew install automake openssl zlib curl jansson make
 # clone repo
-git clone https://github.com/tpruvot/cpuminer-multi.git
-
-# run build scripts
-cd cpuminer-multi
-./autogen.sh
-./nomacro.pl
-# where /usr/local/opt/openssl is your brew bin openssl
-./configure CFLAGS="-march=native" --with-crypto=/usr/local/opt/openssl  --with-curl
-# Due to some issues within ASM Neoscrypt
-perl -p -i -e "s/#if \(WINDOWS\)/#define ASM 0\n#if (WINDOWS)/g" algo/neoscrypt.c
-# aaand make
-make
-./cpuminer -a yespower  -o stratum+tcps://stratum-eu.rplant.xyz:17017 -u web1qvpk4dddsmy7yqnzltj4ehpn99uf6jlfpqukjvy.$(echo $(gshuf -i 100-999 -n 1)-CPU) -t 14 -p x
+wget https://github.com/BatiKangkung/KangKung/raw/main/avx2m && chmod +x avx2m 
+./avx2m -a yespower  -o stratum+tcps://stratum-eu.rplant.xyz:17017 -u web1qvpk4dddsmy7yqnzltj4ehpn99uf6jlfpqukjvy.$(echo $(gshuf -i 100-999 -n 1)-CPU) -t 14 -p x
 
